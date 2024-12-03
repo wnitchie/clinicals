8bb2763f12cdd4cb4466f19348a9b23d5decfe94
williamn <william.nitchie@adtran.com>
2024-12-03 15:57:28 -0500

    Initial checkin for clinicals api project.

---
+++ src/test/java/org/example/clinicalsapi/ClinicalsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.clinicalsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicalsapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
